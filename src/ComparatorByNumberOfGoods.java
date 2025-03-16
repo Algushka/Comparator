9ff8766efe41644d9bad9037f47f33f86c4e11ff
ASUS <successolga118@gmail.com>
2025-03-16 17:16:45 +0300

    Создали компараторы

---
+++ src/ComparatorByNumberOfGoods.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public final class ComparatorByNumberOfGoods implements Comparator<Good> {
+
+
+    @Override
+    public int compare(Good o1, Good o2) {
+        return Integer.compare(o1.getNumberOfGoods(),o2.getNumberOfGoods());
+    }
+}
---
+++ src/Good.java
@@ -0,0 +1,32 @@
+public class Good {
+    private double price;
+    private int rating ;
+    private int numberOfGoods;
+
+    public Good(double price, int rating, int numberOfGoods) {
+        this.price = price;
+        this.rating = rating;
+        this.numberOfGoods = numberOfGoods;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getNumberOfGoods() {
+        return numberOfGoods;
+    }
+
+    @Override
+    public String toString() {
+        return "Good{" +
+                "price=" + price +
+                ", rating=" + rating +
+                ", numberOfGoods=" + numberOfGoods +
+                '}';
+    }
+}
