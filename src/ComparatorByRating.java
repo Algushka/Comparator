17cb53cb70c6e8beb1cf43be411983b31060b2b5
ASUS <successolga118@gmail.com>
2025-03-16 20:56:27 +0300

    Добавили выбор метода сортировки

--- src/ComparatorByRating.java
+++ src/ComparatorByRating.java
@@ -1,2 +1,14 @@
-public class ComparatorByRating {
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ComparatorByRating implements Comparator<Good> {
+
+
+    @Override
+    public int compare(Good o1, Good o2) {
+        return Integer.compare(o1.getRating(),o2.getRating());
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+
+
 //Предположим, вы пишите программу для on-line магазина У каждого товара есть наименование,
 // цена, рейтинг, количество штук на складе и т.д.
 //Ваша программа должна предлагать пользователю выбрать, как бы он хотел сортировать товары
@@ -7,9 +10,8 @@
 //        несколько Comparator<Person>
 
 
-import java.util.Collections;
+import java.util.*;
 import java.util.Comparator;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -21,13 +23,52 @@ goods.add(good1);
 goods.add(new Good(8.3,1,20));
 goods.add(new Good(2.3,3,50));
 goods.add(new Good(7.3,8,90));
+//      List<String> string =new ArrayList<>();/*тренируемся сравнивать String*/
+//      string.add("ручка");
+//      string.add("пенал");
+//     string.add("карандаш");
+//     Collections.sort(string);
+//     System.out.println(string);
+   Comparator <Good> compareByPrice = new ComparatorByPrice();
+   Comparator <Good> compareByRating= new ComparatorByRating();
+        System.out.println("Сортировка по цене по возрастанию ");
+/* можно так  */ // Collections.sort(goods, new ComparatorByPrice());
+/* можно так*/   Collections.sort(goods, compareByPrice);
+        System.out.println(goods);
+/*цена в обр порядке*/ Collections.sort(goods, compareByPrice.reversed());
+        System.out.println("Сортировка по цене в обратном порядке (убыванию-------");
+        System.out.println(goods);
+        System.out.println("Сортировка по рейтингу по возрастанию------ ");
+        Collections.sort(goods,compareByRating);
+        System.out.println(goods);
+        System.out.println("Сортировка по рейтингу по убыванию------ ");
+        Collections.sort(goods,compareByRating.reversed());
+        System.out.println(goods);
+        System.out.println("Сортировка по количеству товара по возрастанию -------------------");
+        Comparator<Good> compareByNumber = new ComparatorByNumberOfGoods();/*новая переменная класса ComparatorByNumberOfFood*/
+        Collections.sort(goods,compareByNumber);
+        System.out.println(goods);
+        System.out.println("Сортировка по количеству товара по убыванию -------------------");
+        Collections.sort(goods,compareByNumber.reversed());
+        System.out.println(goods);
 
-      //  System.out.println(goods);
-        Comparator<Good> byPrice= new ComparatorByPrice();
-  //      List<String> string =new ArrayList<>();/*тренируемся сравнивать String*/
-  //      string.add("ручка");
-   //     string.add("пенал");
-  //      string.add("карандаш");
-   //     System.out.println(string);
-    }
+
+
+}
+public int chooseMethodOfSorting() {
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("Введите номер метода сортировки: ");
+    System.out.println("1- Сортировка по цене по возрастанию: ");
+    System.out.println("2- Сортировка по цене по убыванию: ");
+    System.out.println("3- Сортировка по рейтингу по возрастанию: ");
+    System.out.println("4- Сортировка по рейтингу по убыванию: ");
+    System.out.println("5- Сортировка по номеру по возрастанию: ");
+    System.out.println("6- Сортировка по номеру по возрастанию: ");
+    return scanner.nextInt();
+
+
+
+}
+
+
 }
